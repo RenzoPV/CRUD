218e353670025fe9a0d5a929a1de7d0f209397b6
RenzoPV <67492305+RenzoPV@users.noreply.github.com>
2025-02-22 16:09:45 -0500

    Update CrudApplication.java

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MYSQLHOST: " + System.getenv("MYSQLHOST"));
+		System.out.println("MYSQLPORT: " + System.getenv("MYSQLPORT"));
+		System.out.println("MYSQLDATABASE: " + System.getenv("MYSQLDATABASE"));
+		System.out.println("MYSQLUSER: " + System.getenv("MYSQLUSER"));
+		System.out.println("MYSQLPASSWORD: " + System.getenv("MYSQLPASSWORD"));
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
