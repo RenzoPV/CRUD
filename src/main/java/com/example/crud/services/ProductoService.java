ad0aeb45cd418e732316dcba40625cb6437c14d8
RenzoPV <67492305+RenzoPV@users.noreply.github.com>
2025-02-23 13:18:16 -0500

    Update ProductoService.java

--- src/main/java/com/example/crud/services/ProductoService.java
+++ src/main/java/com/example/crud/services/ProductoService.java
@@ -2,6 +2,7 @@ package com.example.crud.services;
 
 import com.example.crud.entities.Producto;
 import com.example.crud.repositories.ProductoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,15 +22,18 @@ public class ProductoService {
         return repository.findById(id);
     }
 
+    @Transactional
     public Producto guardar(Producto producto) {
         System.out.println("Producto recibido: " + producto);
         return repository.save(producto);
     }
 
+    @Transactional
     public void eliminar(Long id) {
         repository.deleteById(id);
     }
 
+    @Transactional
     public Optional<Producto> actualizar(long id, Producto productoA) {
          return repository.findById(id).map( producto -> {
             producto.setNombre(productoA.getNombre());
