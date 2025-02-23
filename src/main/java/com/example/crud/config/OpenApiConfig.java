e0077131b076b7eb7527df4e27442f3e816ed4f1
RenzoPV <67492305+RenzoPV@users.noreply.github.com>
2025-02-23 17:36:10 -0500

    Create OpenApiConfig.java

---
+++ src/main/java/com/example/crud/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.example.crud.config;
+
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://crud-production-8e41.up.railway.app").description("Servidor en Railway.")
+                ));
+    }
+}
